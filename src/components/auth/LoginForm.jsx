5edd5f06d67ba1a4a6f13f913bd913af3b5aa5e1
mrshaikat <mrshaikat0.cse@gmail.com>
2024-03-07 00:05:50 +0600

    Jwt implmentation

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,46 +1,82 @@
+import axios from "axios";
 import { useForm } from "react-hook-form";
-import Field from "../common/Field";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import Field from "../common/Field";
 
-export default function LoginForm(){
-    const {register, handleSubmit, formState: {errors}} = useForm();
-    const navigate = useNavigate();
-    const {setAuth} = useAuth();
+export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm();
+  const navigate = useNavigate();
+  const { setAuth } = useAuth();
 
-    const submitFOrm = (formData) =>{
-        const user = {...formData};
-
-        console.log("user",user);
-        setAuth({user});
-        navigate('/');
+  const submitFOrm = async (formData) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/auth/login`,
+        formData
+      );
+      if (response.status === 200) {
+        const { token, user } = response.data;
+        if (token) {
+          const authToken = token.token;
+          const refreshToken = token.refreshToken;
+          setAuth({ user, authToken, refreshToken });
+          console.log("atuh");
+          navigate("/");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      setError("root.random", {
+        type: "random",
+        message: `User with email ${formData.email} is not found`,
+      });
     }
-    return (
-       <form className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]" onSubmit={handleSubmit(submitFOrm)}>
-            <Field label="Email" error={errors.email}>
-            <input
-                className={`auth-input ${!!errors?.email ? "border-red-500": "border-gray-200"}`}
-                name="email"
-                type="email"
-                id="email"
-                {...register("email", {required: "Email is required"})}
-                />
-            </Field>
-            <Field label="Password" error={errors.password}>
-            <input
-                className={`auth-input ${!!errors?.password ? "border-red-500": "border-gray-200"}`}
-                name="password"
-                type="password"
-                id="password"
-                {...register("password", {required: "Password is required",minLength: {value: 8, message: "Your Password must be at least 8 characters"}})}
-                />
-            </Field>
-            <Field>
-            <button
-                className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90">
-                Login
-            </button>
-          </Field>
-       </form>
-    );
+  };
+  return (
+    <form
+      className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]"
+      onSubmit={handleSubmit(submitFOrm)}
+    >
+      <Field label="Email" error={errors.email}>
+        <input
+          className={`auth-input ${
+            !!errors?.email ? "border-red-500" : "border-gray-200"
+          }`}
+          name="email"
+          type="email"
+          id="email"
+          {...register("email", { required: "Email is required" })}
+        />
+      </Field>
+      <Field label="Password" error={errors.password}>
+        <input
+          className={`auth-input ${
+            !!errors?.password ? "border-red-500" : "border-gray-200"
+          }`}
+          name="password"
+          type="password"
+          id="password"
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Your Password must be at least 8 characters",
+            },
+          })}
+        />
+      </Field>
+      <p className=" text-red-500 mb-3">{errors?.root?.random?.message}</p>
+      <Field>
+        <button className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90">
+          Login
+        </button>
+      </Field>
+    </form>
+  );
 }
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,11 +1,14 @@
 import HomeIcon from "../../assets/icons/home.svg";
 import Notification from "../../assets/icons/notification.svg";
 import Avatar from "../../assets/images/avatars/avatar_1.jpg";
+import { useAuth } from "../../hooks/useAuth";
 import Logout from "../auth/Logout";
 
 import { Link } from "react-router-dom";
 
+
 const Header = () => {
+  const {auth}= useAuth();
   return (
     <nav className="sticky top-0 z-50 border-b border-[#3F3F3F] bg-[#1E1F24] py-4">
       <div className="container flex flex-col items-center justify-between gap-6 sm:flex-row">
@@ -14,7 +17,9 @@ const Header = () => {
           className="max-w-[100px] lg:max-w-[140px]"
           src={Logo}
           alt="logo" /> */}
-          <h3 className="p-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Facehook</h3>
+          <h3 className="p-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            Facehook
+          </h3>
         </Link>
 
         <div className="flex items-center space-x-4">
@@ -29,7 +34,7 @@ const Header = () => {
           <Logout />
 
           <Link to="/me" className="flex-center !ml-8 gap-3">
-            <span className="text-lg font-medium lg:text-xl">Shaikat</span>
+            <span className="text-lg font-medium lg:text-xl">{auth?.user?.firstName}</span>
             <img
               className="max-h-[32px] max-w-[32px] lg:max-h-[44px] lg:max-w-[44px] rounded-full"
               src={Avatar}
