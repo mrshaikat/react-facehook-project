9408dcc25206c229254a14f2aa406dbf5eb448c4
mrshaikat <mrshaikat0.cse@gmail.com>
2024-03-06 20:06:24 +0600

    guards route setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Login from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import ProfilePage from "./pages/ProfilePage";
 import RegistrationPage from "./pages/RegistrationPage";
+import PrivateRoutes from "./routes/PrivateRoutes";
 function App() {
   return (
    <>
    <Routes>
+    <Route element={<PrivateRoutes/>}>
       <Route element={<HomePage/>} path="/"/>
+      <Route element={<ProfilePage/>} path="/me"/>
+    </Route>
       <Route element={<Login/>} path="/login"/>
       <Route element={<RegistrationPage/>} path="/registration"/>
-      <Route element={<ProfilePage/>} path="/me"/>
       <Route element={<NotFoundPage/>} path="*"/>
    </Routes>
    </>
