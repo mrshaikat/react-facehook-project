b5370f18db09bf8138d63a7506f18d9ba838a35f
mrshaikat <mrshaikat0.cse@gmail.com>
2024-03-06 20:32:14 +0600

    fix initial all avater

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,45 +1,45 @@
+import HomeIcon from "../../assets/icons/home.svg";
+import Notification from "../../assets/icons/notification.svg";
+import Avatar from "../../assets/images/avatars/avatar_1.jpg";
+import Logout from "../auth/Logout";
 
-import HomeIcon from '../../assets/icons/home.svg';
-import Notification from '../../assets/icons/notification.svg';
-import Avatar from '../../assets/images/avatars/avatar_1.png';
-import Logout from '../auth/Logout';
-
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav className="sticky top-0 z-50 border-b border-[#3F3F3F] bg-[#1E1F24] py-4">
-    <div className="container flex flex-col items-center justify-between gap-6 sm:flex-row">
-      <Link to="/">
-        {/* <img
+      <div className="container flex flex-col items-center justify-between gap-6 sm:flex-row">
+        <Link to="/">
+          {/* <img
           className="max-w-[100px] lg:max-w-[140px]"
           src={Logo}
           alt="logo" /> */}
-          Facehook
-      </Link>
-
-      <div className="flex items-center space-x-4">
-        <Link to="/" className="btn-primary">
-          <img src={HomeIcon} alt="Home" />
-          Home
+          <h3 className="p-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Facehook</h3>
         </Link>
-        <button className="icon-btn">
-          <img src={Notification} alt="Notification" />
-        </button>
 
-        <Logout />
-        
-        <Link
-          to="/me"
-          className="flex-center !ml-8 gap-3">
-          <span className="text-lg font-medium lg:text-xl">Shaikat</span>
-          <img className="max-h-[32px] max-w-[32px] lg:max-h-[44px] lg:max-w-[44px]"
-            src={Avatar} alt="avatar" />
-        </Link>
+        <div className="flex items-center space-x-4">
+          <Link to="/" className="btn-primary">
+            <img src={HomeIcon} alt="Home" />
+            Home
+          </Link>
+          <button className="icon-btn">
+            <img src={Notification} alt="Notification" />
+          </button>
+
+          <Logout />
+
+          <Link to="/me" className="flex-center !ml-8 gap-3">
+            <span className="text-lg font-medium lg:text-xl">Shaikat</span>
+            <img
+              className="max-h-[32px] max-w-[32px] lg:max-h-[44px] lg:max-w-[44px] rounded-full"
+              src={Avatar}
+              alt="avatar"
+            />
+          </Link>
+        </div>
       </div>
-    </div>
-  </nav>
-  )
-}
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,25 +1,25 @@
-import { useAuth } from "../hooks/useAuth"
-import { Outlet, Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Header from "../components/common/Header";
+import { useAuth } from "../hooks/useAuth";
 const PrivateRoutes = () => {
   const { auth } = useAuth();
 
   return (
     <>
-        {
-            auth.user ? (
-                <main className="mx-auto max-w-[1020px] py-8">
-                    <div className="container">
-                        <Header />
-                        <Outlet />
-                    </div>
-                </main>
-            ) : (
-                <Navigate to="/login" />
-            )
-        }
+      {auth.user ? (
+        <>
+          <Header />
+          <main className="mx-auto max-w-[1020px] py-8">
+            <div className="container">
+              <Outlet />
+            </div>
+          </main>
+        </>
+      ) : (
+        <Navigate to="/login" />
+      )}
     </>
-  )
-}
+  );
+};
 
 export default PrivateRoutes;
